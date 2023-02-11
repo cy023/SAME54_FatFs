b5f9f8c18a311706fe3656a42cbc4d3c3fb882b4
cyyang <cyyang023@gmail.com>
2023-02-11 23:48:52 +0800

    Add SAME54_GCC project template
    https://github.com/cy023/SAME54_GCC

---
+++ Drivers/HAL/syscall.c
@@ -0,0 +1,31 @@
+/**
+ * @file syscall.c
+ * @author cy023
+ * @date 2022.08.02
+ * @brief
+ *
+ */
+
+#include "uart.h"
+
+int _write(__attribute__((unused)) int fd, char *ptr, int len)
+{
+    for (int i = 0; i < len; i++) {
+        uart0_putc(*ptr++);
+        if (*ptr == '\n')
+            uart0_putc('\r');
+    }
+    return len;
+}
+
+int _read(__attribute__((unused)) int fd, char *ptr, int len)
+{
+    for (int i = 0; i < len; i++)
+        *ptr++ = uart0_getc();
+    return len;
+}
+
+void _ttywrch(int ch)
+{
+    uart0_putc(ch);
+}
